b2c8eb07fba641182a399445a71345dc6e33a2a5
RizwandaaK <rizwanda_keysha_29rpl@student.smktelkom-mlg.sch.id>
2021-02-15 13:59:11 +0700

    Praktikum2

---
+++ src/Praktikum2/Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Praktikum2;
+
+/**
+ *
+ * @author rizwa
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+    Pohon cemara = new Pohon();
+    cemara.setNama();
+    cemara.setTinggi();
+    cemara.getNama();
+    cemara.getTinggi();
+    
+    Pohon kelapa = new Pohon();
+    kelapa.setNama();
+    kelapa.setTinggi();
+    kelapa.getNama();
+    kelapa.getTinggi();
+    }
+}
+
